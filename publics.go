8eda835ee8784dd8d5baafe065055a452101e72b
Hedzr Yeh <hedzrz@gmail.com>
2019-12-26 22:08:27 +0800

    fixed ci building
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- publics.go
+++ publics.go
@@ -3,7 +3,6 @@
 package errors
 
 import (
-	"errors"
 	"runtime"
 	"strings"
 )
@@ -99,19 +98,6 @@ func HasInnerErrors(err error) (yes bool) {
 func HasAttachedErrors(err error) (yes bool) {
 	if ex, ok := err.(interface{ HasAttachedErrors() bool }); ok {
 		return ex.HasAttachedErrors()
-	}
-	return false
-}
-
-// HasWrappedError detects if nested or wrapped errors present
-//
-// nested error: ExtErr.inner
-// wrapped error: fmt.Errorf("... %w ...", err)
-func HasWrappedError(err error) (yes bool) {
-	if ex, ok := err.(interface{ GetNestedError() *ExtErr }); ok {
-		return ex.GetNestedError() != nil
-	} else if errors.Unwrap(err) != nil {
-		return true
 	}
 	return false
 }
