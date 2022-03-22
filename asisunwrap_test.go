414a08f9d226f86441dabbe272c8087868c7d87d
Hedzr Yeh <hedzrz@gmail.com>
2022-03-22 11:08:53 +0800

    fix: make Is() work for go1.12 and below, eg:
    ```go
    _, err := strconv.ParseFloat("hello", 64)
    assert(errors.Is(err, strconv.ErrSyntax), "should be equal")
    ```

--- asisunwrap_test.go
+++ asisunwrap_test.go
@@ -1,9 +1,30 @@
 package errors
 
 import (
+	// errors2 "errors"
 	"io"
+	"strconv"
 	"testing"
 )
+
+// TestErrorsIs _
+func TestErrorsIs(t *testing.T) {
+	_, err := strconv.ParseFloat("hello", 64)
+	t.Logf("err = %+v", err)
+
+	// e1 := errors2.Unwrap(err)
+	// t.Logf("e1 = %+v", e1)
+
+	t.Logf("errors.Is(err, strconv.ErrSyntax): %v", Is(err, strconv.ErrSyntax))
+	t.Logf("errors.Is(err, &strconv.NumError{}): %v", Is(err, &strconv.NumError{}))
+
+	var e2 *strconv.NumError
+	if As(err, &e2) {
+		t.Logf("As() ok, e2 = %v", e2)
+	} else {
+		t.Logf("As() not ok")
+	}
+}
 
 func TestIs(t *testing.T) {
 
