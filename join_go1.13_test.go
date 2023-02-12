acafe50dc4adca34e1bdec1b18ef4b9cfab507bc
Hedzr Yeh <hedzrz@gmail.com>
2023-02-12 12:22:21 +0800

    fix test because format '%w' cannot work for go1.11

--- join_go1.13_test.go
+++ join_go1.13_test.go
@@ -1,5 +1,8 @@
 // Copyright © 2023 Hedzr Yeh.
 
+//go:build go1.13
+// +build go1.13
+
 package errors_test
 
 import (
@@ -9,7 +12,7 @@ import (
 	"gopkg.in/hedzr/errors.v3"
 )
 
-func TestJoinErrors(t *testing.T) {
+func TestJoinErrorsStdFormat(t *testing.T) {
 	err1 := errors.New("err1")
 	err2 := errors.New("err2")
 	err := errors.Join(err1, err2)
@@ -23,5 +26,18 @@ func TestJoinErrors(t *testing.T) {
 		t.Log("err is err2")
 	} else {
 		t.Fatal("expecting err is err2")
+	}
+
+	err3 := fmt.Errorf("error3: %w", err)
+	fmt.Printf("%T, %v\n", err3, errors.Unwrap(err3))
+	if errors.Is(err3, err1) {
+		t.Log("err3 is err1")
+	} else {
+		t.Fatal("expecting err3 is err1")
+	}
+	if errors.Is(err3, err2) {
+		t.Log("err3 is err2")
+	} else {
+		t.Fatal("expecting err3 is err1")
 	}
 }
