cda071d7be31bbabfe2ab0a4c04e65a8326cf8cf
Hedzr Yeh <hedzrz@gmail.com>
2023-02-26 19:12:48 +0800

    fix TestJoinErrorsStdFormat
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- join_go1.13_test.go
+++ join_go1.13_test.go
@@ -15,29 +15,33 @@ import (
 )
 
 func TestJoinErrorsStdFormat(t *testing.T) {
-	err1 := errors.New("err1")
-	err2 := errors.New("err2")
-	err := errors.Join(err1, err2)
+	err1 := v3.New("err1")
+	err2 := v3.New("err2")
+	err := v3.Join(err1, err2)
 	fmt.Printf("%T, %v\n", err, err)
-	if errors.Is(err, err1) {
+	
+	if v3.Is(err, err1) {
 		t.Log("err is err1")
 	} else {
 		t.Fatal("expecting err is err1")
 	}
-	if errors.Is(err, err2) {
+	
+	if v3.Is(err, err2) {
 		t.Log("err is err2")
 	} else {
 		t.Fatal("expecting err is err2")
 	}
 
 	err3 := fmt.Errorf("error3: %w", err)
-	fmt.Printf("%T, %v\n", err3, errors.Unwrap(err3))
-	if errors.Is(err3, err1) {
+	fmt.Printf("%T, %v\n", err3, v3.Unwrap(err3))
+	
+	if v3.Is(err3, err1) {
 		t.Log("err3 is err1")
 	} else {
 		t.Fatal("expecting err3 is err1")
 	}
-	if errors.Is(err3, err2) {
+	
+	if v3.Is(err3, err2) {
 		t.Log("err3 is err2")
 	} else {
 		t.Fatal("expecting err3 is err1")
